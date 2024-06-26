5e502d46f4461ac47557d8b616b9245061aae394
hikaru0331 <90139340+hikaru0331@users.noreply.github.com>
2024-06-26 13:47:51 +0900

    [add]ビルドテスト

--- Assets/Scripts/PUN2Sample/SampleScene.cs
+++ Assets/Scripts/PUN2Sample/SampleScene.cs
@@ -1,22 +1,29 @@
+using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace PUN2Sample
 {    
-    public class SampleScene : MonoBehaviour
+    public class SampleScene : MonoBehaviourPunCallbacks
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-            
+        private void Start() {
+            // PhotonServerSettingsの設定内容を使ってマスターサーバーへ接続する
+            PhotonNetwork.ConnectUsingSettings();
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-            
+        // マスターサーバーへの接続が成功した時に呼ばれるコールバック
+        public override void OnConnectedToMaster() {
+            // "Room"という名前のルームに参加する（ルームが存在しなければ作成して参加する）
+            PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions(), TypedLobby.Default);
+        }
+
+        // ゲームサーバーへの接続が成功した時に呼ばれるコールバック
+        public override void OnJoinedRoom() {
+            // ランダムな座標に自身のアバター（ネットワークオブジェクト）を生成する
+            var position = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+            PhotonNetwork.Instantiate("Avatar", position, Quaternion.identity);
         }
     }
 }
-

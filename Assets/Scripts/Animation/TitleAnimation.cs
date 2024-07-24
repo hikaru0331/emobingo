9a98f87932389797e775dd1071872686d3574ec9
168momo <wakame1317@outlook.jp>
2024-07-24 14:18:16 +0900

    [add]入室パネルもスライドイン

--- Assets/Scripts/Animation/TitleAnimation.cs
+++ Assets/Scripts/Animation/TitleAnimation.cs
@@ -10,7 +10,7 @@ public class TitleAnimation : MonoBehaviour
     public RectTransform JoinButton;
     public RectTransform MakeButton;
     public RectTransform RoomJoinPanel;
-    public RectTransform RoomCrePanel;
+    public RectTransform RoomCreatePanel;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +27,14 @@ public class TitleAnimation : MonoBehaviour
     public void RoomJoinOffAnimation()
     {
         RoomJoinPanel.DOMoveX(3.0f, 1.5f).SetEase(Ease.OutQuint);
+    }
+
+    public void RoomCreateAnimation()
+    {
+        RoomCreatePanel.DOMoveX(0, 1.5f).SetEase(Ease.OutQuint);
+    }
+    public void RoomCreateOffAnimation()
+    {
+        RoomCreatePanel.DOMoveX(3.0f, 1.5f).SetEase(Ease.OutQuint);
     }
 }

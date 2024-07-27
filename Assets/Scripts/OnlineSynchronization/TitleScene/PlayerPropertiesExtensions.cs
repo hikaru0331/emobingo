89d0962a824590ca1da56056e4cde19717dd4226
hikaru0331 <17731106.koga.cgp@gmail.com>
2024-07-28 02:05:58 +0900

    [WIP]ルームのカスタムプロパティ用の拡張メソッド途中

---
+++ Assets/Scripts/OnlineSynchronization/TitleScene/PlayerPropertiesExtensions.cs
@@ -0,0 +1,26 @@
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+
+    public static class RoomPropertiesExtensions
+    {
+        private const string PrizesKey = "Prizes";
+
+        public static void SetPrizesMaximun(this Hashtable hashtable, int prizes)
+        {
+            hashtable["Prizes"] = prizes;
+        }
+        
+        // private const string RankKey = "Rank";
+
+        // // Hashtableにプレイヤーのランクを設定する
+        // public static void SetPlayerRank(this Hashtable hashtable, Player player)
+        // {
+        //     hashtable[RankKey] = player.GetRank();
+        // }
+
+        // // ロビーから取得できるカスタムプロパティのキーの配列を返す
+        // public static string[] KeysForLobby(this Hashtable hashtable)
+        // {
+        //     return new[] { RankKey };
+        // }
+    }

1a88a0d05a3f3ee3acf6c30c4bba5fe77a29f786
168momo <wakame1317@outlook.jp>
2024-07-31 15:45:14 +0900

    [fix]ビンゴ時にアニメーションが実行されるよう修正

--- Assets/Scripts/BINGO/BingoManager.cs
+++ Assets/Scripts/BINGO/BingoManager.cs
@@ -126,6 +126,7 @@ public class BingoManager : MonoBehaviour
     }
     public void NewGame()
     {
+        OnChangeSubInfoText?.Invoke("");
         StartCoroutine(StartNewGame());
     }
     
@@ -216,6 +217,7 @@ public class BingoManager : MonoBehaviour
             // ちなみにTextMeshProの関係で、日本語は使えないぞ！
             OnChangeSubInfoText?.Invoke("BINGO");
             Debug.Log("Bingo!(Nextメソッドのログを修正)");
+            FindObjectOfType<BingoTextAnimation>().textAnime();
             bingoLog = false;
         }
     }

992a983706036bb97f84aedbcb40fc5d3a03b448
hikaru0331 <90139340+hikaru0331@users.noreply.github.com>
2024-07-31 14:08:03 +0900

    [add]ルーム入室機能実装

--- Assets/Scripts/OnlineSynchronization/TitleScene/RoomJoin.cs
+++ Assets/Scripts/OnlineSynchronization/TitleScene/RoomJoin.cs
@@ -4,70 +4,61 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RoomCreate : MonoBehaviourPunCallbacks
+public class RoomJoin : MonoBehaviourPunCallbacks
 {
     [SerializeField] private TMP_InputField roomIdImputField = default;
     [SerializeField] private TMP_InputField userNameImputField = default;
-    [SerializeField] private TMP_InputField prizesInputField = default;
 
-    [SerializeField] private Button createRoomButton = default;
+    [SerializeField] private Button joinRoomButton = default;
 
     [SerializeField] private GameObject ThemePanel;
 
     private void Start()
     {
         // パスワードを入力する前は、ルーム参加ボタンを押せないようにする
-        createRoomButton.interactable = false;
+        joinRoomButton.interactable = false;
 
         //　ルーム作成ボタンのインタラクティブを設定するためのリスナーを登録
         roomIdImputField.onValueChanged.AddListener(OnInputFieldsValueChanged);
         userNameImputField.onValueChanged.AddListener(OnInputFieldsValueChanged);
-        prizesInputField.onValueChanged.AddListener(OnInputFieldsValueChanged);
 
-        createRoomButton.onClick.AddListener(OnConfirmButtonClicked);
+        joinRoomButton.onClick.AddListener(OnConfirmButtonClicked);
     }
 
     private void OnInputFieldsValueChanged(string value)
     {
-        // 各フィールドに値が入力されている時のみ、ルーム作成ボタンを押せるようにする
-        if (userNameImputField.text != "" && prizesInputField.text != "")
+        // ユーザー名入力フィールドに値が入力されている時のみ、ルーム作成ボタンを押せるようにする
+        if (userNameImputField.text != "")
         {
             // ルーム番号が4桁入力されている時のみ、ルーム作成ボタンを押せるようにする
-            createRoomButton.interactable = (roomIdImputField.text.Length == 4);
+            joinRoomButton.interactable = (roomIdImputField.text.Length == 4);
         }
         else
         {
-            createRoomButton.interactable = false;
+            joinRoomButton.interactable = false;
         }
     }
 
     private void OnConfirmButtonClicked()
     {
         // ルーム参加処理中は、入力できないようにする
-        createRoomButton.interactable = false;
+        joinRoomButton.interactable = false;
 
         // プレイヤーのカスタムプロパティにユーザー名を設定
         PhotonNetwork.LocalPlayer.SetUserName(userNameImputField.text);
 
         // ルーム番号と同じ名前のルームを作成する
-        PhotonNetwork.CreateRoom(roomIdImputField.text);
+        PhotonNetwork.JoinRoom(roomIdImputField.text);
     }
 
-    public override void OnCreatedRoom()
+    public override void OnJoinedRoom()
     {
-        // ルームのカスタムプロパティに景品の最大数を設定
-        PhotonNetwork.CurrentRoom.SetPrizesMaximun(int.Parse(prizesInputField.text));
-         
-        // 景品数のプロパティがうまく設定されてなさそう…？
-        Debug.Log(int.Parse(prizesInputField.text));
-        Debug.Log(PhotonNetwork.CurrentRoom.GetPrizesMaximun());
-
         // ルームへの参加が成功したら、次のパネルを表示する
         gameObject.SetActive(false);
         ThemePanel.SetActive(true);
     }
 
-    public override void OnCreateRoomFailed(short returnCode, string message)
+    public override void OnJoinRoomFailed(short returnCode, string message)
     {
         // ルームへの参加が失敗したら、パスワードを再び入力できるようにする
         roomIdImputField.text = string.Empty;
